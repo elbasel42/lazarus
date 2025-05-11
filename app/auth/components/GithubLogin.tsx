cac499103a2feab4f61b7ab20e6af2bd4a58fb29
elbasel42 <abdelrahman.elbasel42@gmail.com>
2025-05-11 21:23:33 +0300

    fix styles

--- app/auth/components/GithubLogin.tsx
+++ app/auth/components/GithubLogin.tsx
@@ -3,7 +3,7 @@ import { githubLogin } from "@auth/actions";
 export const GithubLogin = () => {
   return (
     <form action={githubLogin}>
-      <button type="submit" className="primary">
+      <button type="submit" className="primary px-1 py-2 w-full">
         Login with Github
       </button>
     </form>
--- app/auth/components/GoogleLogin.tsx
+++ app/auth/components/GoogleLogin.tsx
@@ -3,7 +3,7 @@ import { googleLogin } from "@auth/actions";
 export const GoogleLogin = () => {
   return (
     <form action={googleLogin}>
-      <button type="submit" className="primary">
+      <button type="submit" className="primary px-1 py-2 w-full">
         Login with Google
       </button>
     </form>
