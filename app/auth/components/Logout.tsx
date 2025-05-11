7eb985d5669ff8896a1f5b9aa43a179f83b452ee
elbasel42 <abdelrahman.elbasel42@gmail.com>
2025-05-11 21:38:39 +0300

    make navbar a server component

--- app/auth/components/Logout.tsx
+++ app/auth/components/Logout.tsx
@@ -3,7 +3,7 @@ import { logout } from "@auth/actions";
 export const Logout = () => {
   return (
     <form action={logout}>
-      <button type="submit" className="primary">
+      <button type="submit" className="primary w-full px-1 py-2">
         Logout
       </button>
     </form>
--- app/components/index.ts
+++ app/components/index.ts
@@ -1,3 +1,4 @@
 export { UserImage } from "./UserImage";
 export { NavBar } from "./Navbar";
 export { Net } from "./Net";
+export { Slash } from "./Slash";
