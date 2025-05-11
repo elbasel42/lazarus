7d7f216448413b8289771c815945b6000e219fa6
elbasel42 <abdelrahman.elbasel42@gmail.com>
2025-05-11 21:07:04 +0300

    fix styles

--- app/(pages)/home/page.tsx
+++ app/(pages)/home/page.tsx
@@ -2,26 +2,28 @@ import { Net } from "@components";
 
 export default function Home() {
   return (
-    <main>
+    <>
       <Net />
-      <h1 className="text-center pt-24 text-2xl font-bold">
-        Your future in Tech awaits!
-      </h1>
-      <p className="text-center">
-        Make{" "}
-        <span className="underline decoration-wavy decoration-purple-800">
-          35,000 EGP
-        </span>{" "}
-        a month!
-      </p>
-      <div className="flex gap-4 !bg-black/5 w-fit mx-auto">
-        <button className="primary !w-fit mt-4 !rounded-full">
-          Get Started
-        </button>
-        <button className="primary !w-fit mt-4 !rounded-full !bg-black !text-white ">
-          Contact Us
-        </button>
-      </div>
-    </main>
+      <main>
+        <h1 className="pt-24 text-2xl font-bold text-center">
+          Your future in Tech awaits!
+        </h1>
+        <p className="text-center">
+          Make{" "}
+          <span className="underline decoration-wavy decoration-purple-800">
+            35,000 EGP
+          </span>{" "}
+          a month!
+        </p>
+        <div className="flex gap-4 !bg-black/5 w-fit mx-auto mt-4">
+          <button className="!w-[20vw] primary !rounded-full">
+            Get Started
+          </button>
+          <button className="!w-[20vw] primary border border-white/20 !rounded-full !bg-black !text-white ">
+            Contact Us
+          </button>
+        </div>
+      </main>
+    </>
   );
 }
