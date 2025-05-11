0890b5fc41d3074d7206b817fb1e8d860166ba2e
elbasel42 <abdelrahman.elbasel42@gmail.com>
2025-05-11 12:13:14 +0300

    minor styling

--- app/storage/test/page.tsx
+++ app/storage/test/page.tsx
@@ -29,26 +29,26 @@ const TestStoragePage = async () => {
         <button className="button button-danger" formAction={deleteAllData}>
           Delete all data
         </button>
-        <table className="table w-full">
-          <thead className="table-header">
-            <tr className="table-row">
-              <th className="table-head text-green-600">key</th>
-              <th className="table-head text-red-600">value</th>
-            </tr>
-          </thead>
-          <tbody className="table-body">
-            {allKeys.map(async (key) => {
-              const value = await getValue(key);
-              return (
-                <tr className="table-row">
-                  <td className="table-data">{key}</td>
-                  <td className="table-data">{value?.toString()}</td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
       </form>
+      <table className="table w-full mt-4">
+        <thead className="table-header">
+          <tr className="table-row">
+            <th className="table-head text-green-600">key</th>
+            <th className="table-head text-red-600">value</th>
+          </tr>
+        </thead>
+        <tbody className="table-body">
+          {allKeys.map(async (key) => {
+            const value = await getValue(key);
+            return (
+              <tr className="table-row">
+                <td className="table-data">{key}</td>
+                <td className="table-data">{value?.toString()}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </main>
   );
 };
